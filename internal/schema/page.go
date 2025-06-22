b45dc4393b6c76b7d82c0bd4991808834a8fbb33
Wakisa Mwabulambo <wakielambo@gmail.com>
2025-06-22 20:22:30 +0200

    feature:created page request struct

---
+++ internal/schema/page.go
@@ -0,0 +1,25 @@
+package schema
+
+import (
+	"database/sql"
+
+	"github.com/gobuffalo/nulls"
+)
+
+type PageRequest struct {
+	ParentPageID       sql.NullInt32 `json:"parentPageId"`
+	PageVersion        string        `json:"pageVersion"`
+	OrgID              int32         `json:"orgId"`
+	ProjectID          int32         `json:"projectId"`
+	Code               string        `json:"code"`
+	Title              string        `json:"title"`
+	FilePath           nulls.String  `josn:"filePath"`
+	Content            string        `json:"content"`
+	PageType           string        `json:"pageType"`
+	MimeType           string        `json:"mimeType"`
+	HasEmbeddedMedia   bool          `json:"hasEmbeddedMedia"`
+	ExternalContentUrl nulls.String  `json:"externalContentUrl"`
+	NotionUrl          nulls.String  `json:"notionUrl"`
+	LastEditedBy       int32         `json:"lastEditedBy"`
+	CreatedBy          int32         `json:"createdBy"`
+}
