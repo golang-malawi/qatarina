54a1617ff4681a0d36be901716ecb4fae8d1dd64
Zikani Nyirenda Mwase <zikani.nmwase@ymail.com>
2025-06-28 12:07:50 +0200

    fix(ui): fixed general navigation and project route (#29)
    - load project data on project route so that header is displayed on all sub-pages
    - added header to create project page
    - made inbox a route page as appropriate
    - removed create new test-case on inbox page
    - commented out integrations and reports on main-navigation and replaced with settings
    - made most headers Heading with 3xl level
    
    Signed-off-by: Zikani Nyirenda Mwase <zikani.nmwase@ymail.com>

--- ui/src/routes/(app)/settings/index.tsx
+++ ui/src/routes/(app)/settings/index.tsx
@@ -1,3 +1,4 @@
+import { Box, Heading } from "@chakra-ui/react";
 import { createFileRoute } from "@tanstack/react-router";
 
 export const Route = createFileRoute("/(app)/settings/")({
@@ -6,8 +7,8 @@ export const Route = createFileRoute("/(app)/settings/")({
 
 function RouteComponent() {
   return (
-    <div>
-      <h1>Settings</h1>
-    </div>
+    <Box>
+      <Heading size="3xl">Settings</Heading>
+    </Box>
   );
 }
