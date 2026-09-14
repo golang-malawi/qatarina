f253e8298a436ef9d3c3ae9f9922672213ce107c
hopesain <hopessain@gmail.com>
2026-09-14 14:04:18 +0200

    created the routes file exporting a file

--- internal/schema/testcase_export.go
+++ internal/schema/testcase_export.go
@@ -1,15 +1,11 @@
 package schema
 
-// ExportTestCasesRequest is the request body for exporting test cases
-// to a downloadable file (CSV or XLSX).
 type ExportTestCasesRequest struct {
-	FileName   string              `json:"filename"`
-	FileFormat string              `json:"fileformat"`
-	TestCases  []ExportedTestCase  `json:"testcases" validate:"required,min=1"`
+	FileName   string             `json:"filename"`
+	FileFormat string             `json:"fileformat"`
+	TestCases  []ExportedTestCase `json:"testcases" validate:"required,min=1"`
 }
 
-// ExportedTestCase mirrors services.TestCase but carries JSON tags for
-// decoding the user-supplied export request.
 type ExportedTestCase struct {
 	Title           string   `json:"title"`
 	Description     string   `json:"description"`
